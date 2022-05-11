74ffe50bf3846a54c67d2629498f8ea587686b13
Jean-Nyckolas Roy <jnroy27@gmail.com>
2022-05-10 21:28:09 -0400

    Rquête de connexion

--- app/src/main/java/com/example/projetmobile/LoginActivity.java
+++ app/src/main/java/com/example/projetmobile/LoginActivity.java
@@ -14,6 +14,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -46,16 +47,30 @@ public class LoginActivity extends AppCompatActivity {
                 else {
                     queue = Volley.newRequestQueue(LoginActivity.this);
 
-                    String url = "http://192.168.2.13:8000/api/compareEmail/"+email;
+                    String url = "http://192.168.2.13:8000/api/login";
+                    JSONObject jsonBody = new JSONObject();
+
+                    try {
+                        jsonBody.put("email", email);
+                        jsonBody.put("mdp", password);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
 
                     // Request a string response from the provided URL.
-                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                            new Response.Listener<String>() {
+                    JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody,
+                            new Response.Listener<JSONObject>() {
                                 @Override
-                                public void onResponse(String response) {
-                                    // Reste à traiter la réponse
-                                    response = response.substring(response.indexOf('\"')).replace("\"", "");
-                                    Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+                                public void onResponse(JSONObject response) {
+                                    if (response.has("err")) {
+                                        String error = "";
+                                        try {
+                                            error = response.get("err").toString();
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                        Toast.makeText(LoginActivity.this, error, Toast.LENGTH_SHORT).show();
+                                    }
                                 }
                             },
                             new Response.ErrorListener() {
@@ -66,7 +81,7 @@ public class LoginActivity extends AppCompatActivity {
                             });
 
                     // Add the request to the RequestQueue.
-                    queue.add(stringRequest);
+                    queue.add(objectRequest);
                 }
                 // À envoyer vers l'accueil
                 /*Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
